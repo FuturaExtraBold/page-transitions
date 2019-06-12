cc1fcc952079aa0934fe6f5eef0206c2336bb9b9
Ben Hays <ben@kajabi.com>
2019-06-12 12:32:01 -0700

    Added stylesheets.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Page Transitions</h1>
-    </div>
-  );
+import Home from "./Home";
+import About from "./About";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <section className="navigation">
+            <div className="container">
+              <NavLink exact to="/" className="navigation__link" activeClassName="navigation__link--active">Home</NavLink>
+              <NavLink to="/about" className="navigation__link" activeClassName="navigation__link--active">About</NavLink>
+            </div>
+          </section>
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/about" component={ About } />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./stylesheets/all.scss";
+
 import App from "./components/App";
 
 ReactDOM.render(
