fec839d0192d05c6ec8eb7ba176979299cf213a7
Ben Hays <ben@kajabi.com>
2019-06-13 10:59:43 -0700

    Got animation working properly.

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { TransitionGroup, Transition } from "react-transition-group";
+
+import $ from "jquery";
+import { TweenMax, TimelineMax } from "gsap";
 
 import About from "./About";
 import Contact from "./Contact";
 import Home from "./Home";
 import Layout from "./Layout";
 
-const App = (props) => {
-  const currentKey = window.location.pathname.split('/')[1] || '/';
-  const timeout = { enter: 300, exit: 200 };
-  console.log("this.props.location:", props.location, "currentKey:", currentKey, "timeout:", timeout);
-  return (
-    <Layout>
-      <TransitionGroup>
-        <Transition
-          key={ currentKey }
-          timeout={ timeout }
-          mountOnEnter={ true }
-          unmountOnExit={ true }
-          onEnter={ (node) => { console.log("enter", node); }}
-          onExit={ (node) => { console.log("exit", node); }}
-        >
-          <div className="content">
-            <Switch location={ props.location }>
+class App extends Component {
+
+  componentDidMount() {
+    console.log("App componentDidMount");
+
+    this.overlay = $(".overlay");
+    this.tl = new TimelineMax();
+    this.animationDuration = 0.5;
+
+    this.tl.set(this.overlay, { x: "-100%" });
+  }
+
+  onEnter = (node) => {
+    this.tl.set(node, { display: "none" });
+    this.tl.fromTo(this.overlay, this.animationDuration, { x: "-100%" }, { x: "0%", ease: "easeInOutExpo" });
+    this.tl.set(node, { display: "block" });
+    this.tl.to(this.overlay, this.animationDuration, { x: "100%", ease: "easeInOutExpo" });
+  }
+
+  onExit = (node) => {
+    TweenMax.set(node, { display: "none", delay: this.animationDuration });
+  }
+
+  render() {
+    const currentKey = window.location.pathname.split('/')[1] || '/';
+    return (
+      <Layout>
+        <TransitionGroup>
+          <Transition
+            key={ currentKey }
+            timeout={ 2000 }
+            onEnter={ this.onEnter }
+            onExit={ this.onExit }
+          >
+            <Switch location={ this.props.location }>
               <Route exact path="/" component={ Home } />
               <Route exact path="/about" component={ About } />
               <Route exact path="/contact" component={ Contact } />
             </Switch>
-          </div>
-        </Transition>
-      </TransitionGroup>
-    </Layout>
-  );
+          </Transition>
+        </TransitionGroup>
+      </Layout>
+    );
+  }
 }
 
 export default App;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,10 +5,11 @@ import Navigation from "./Navigation";
 const Layout = (props) => {
   return (
     <Fragment>
-    <Navigation />
-    <div className="App">
-      { props.children }
-    </div>
+      <Navigation />
+      <div className="App">
+        { props.children }
+        <div className="overlay"></div>
+      </div>
     </Fragment>
   );
 }
